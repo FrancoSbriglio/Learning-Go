98059bd23299d7647b3d98abce7c302314645f06
A446647 <A446647@santandertecnologia.com.ar>
2023-03-22 00:56:16 -0300

    Como pasar parametros por Url

--- Api-Rest/main.go
+++ Api-Rest/main.go
@@ -12,7 +12,8 @@ func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index)
-	router.HandleFunc("/contacto", Contact)
+	router.HandleFunc("/peliculas", MovieList)
+	router.HandleFunc("/peliculas/{id}", MovieShow)
 	server := http.ListenAndServe(":8080", router) //escucha y sirve para que el servidor se levante indicando el puerto
 
 	log.Fatal(server)
@@ -22,6 +23,14 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hola mundo desde mi servidor web con GO")
 }
 
-func Contact(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Esta es la pagina de contacto")
+func MovieList(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Listado de peliculas")
+}
+
+func MovieShow(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	movie_id := params["id"]
+
+	fmt.Fprintf(w, "Has cargo la pelicula numero %s", movie_id)
 }
