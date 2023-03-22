380671f9cf8c52b9f5b488bad2ed0abf771b059f
A446647 <A446647@santandertecnologia.com.ar>
2023-03-22 00:37:10 -0300

    Creando y probando rutas con gorilamux

--- Api-Rest/main.go
+++ Api-Rest/main.go
@@ -4,16 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { //w response writer, r request
-		fmt.Fprintf(w, "Hola mundo desde mi servidor web con go")
-	}) //Creando el servidor, esto nos sirve para crear una ruta, que va a ser la ruta principal de la aplicacion o del servidor
 
-	fmt.Println("El servidor deberia estar corriendo en http://localhost:8080")
-	server := http.ListenAndServe(":8080", nil) //escucha y sirve para que el servidor se levante indicando el puerto
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/contacto", Contact)
+	server := http.ListenAndServe(":8080", router) //escucha y sirve para que el servidor se levante indicando el puerto
 
 	log.Fatal(server)
 
+}
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hola mundo desde mi servidor web con GO")
+}
+
+func Contact(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Esta es la pagina de contacto")
 }
